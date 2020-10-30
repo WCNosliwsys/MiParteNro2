602b57f4445904b182f4c0cc8cbf3b2c1bcf763f
Wilson Callisaya <demarktec@gmail.com>
2020-10-29 20:14:17 -0500

    Añadiendo trabajo de hilos secundario para la carga del sonido remoto de Multimedia3 con el JobIntentService

---
+++ app/src/main/java/com/example/miparte2/MiJobIntentServiceReproduccion.java
@@ -0,0 +1,25 @@
+package com.example.miparte2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.SystemClock;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.JobIntentService;
+
+import java.io.IOException;
+
+public class MiJobIntentServiceReproduccion extends JobIntentService {
+    static final int JOB_ID = 1000;
+    static void enqueueWork(Context context, Intent work) {
+        enqueueWork(context, MiJobIntentServiceReproduccion.class, JOB_ID, work);
+    }
+    @Override
+    protected void onHandleWork(@NonNull Intent intent) {
+        try {
+            Multimedia3.mediaPlayer.setDataSource("https://migrandiosaappwilson.000webhostapp.com/dancing.mp3");
+            Multimedia3.mediaPlayer.prepare();
+        } catch (IOException e) {
+        }
+    }
+}
